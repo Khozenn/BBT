05e9d78ddca9148fecee679ba79914ed6396e4d9
rjubault <romainjubault@gmail.com>
2020-02-05 14:45:52 +0000

    Création du dossier server-led

--- led/led.js
+++ led/led.js
@@ -1,7 +1,8 @@
 const Gpio = require('onoff').Gpio;
 const sleep = require('sleep');
-// Le nombre passé en paramètre correspond au numéro de GPIO et non au numéro de la pin.
-const led = new Gpio(4, 'out');
+//Création d'une variable qui va nous permettre d'accéder à un GPIO du raspberry  
+//⚠️ Le nombre passé en paramètre correspond au numéro de GPIO et non au numéro de la pin.
+const led = new Gpio(17, 'out');
 
 console.log('Led On');
 //On indique à la pin GPIO 4 que l'on veut envoyer du courant sur celle-ci
