6a91fb11f31a295c005a425aa9190a59aadfa3be
Khozenn <bonenfant.damien56@gmail.com>
2020-02-05 15:50:32 +0100

    essai du on

--- tp_2/server.js
+++ tp_2/server.js
@@ -42,6 +42,24 @@ app.get('/pooc', (request, response) => {
 response.send('Bienvenue sur la page pooc')
 })
 
+app.get('/on', (request, response) => {
+  const Gpio = require('onoff').Gpio;
+  const sleep = require('sleep');
+  //Création d'une variable qui va nous permettre d'accéder à un GPIO du raspberry  
+  //⚠️ Le nombre passé en paramètre correspond au numéro de GPIO et non au numéro de la pin.
+  const led = new Gpio(17, 'out');
+  
+  console.log('Led On');
+  //On indique à la pin GPIO 4 que l'on veut envoyer du courant sur celle-ci
+  led.writeSync(1);
+ 
+  console.log('Led Off');
+  
+  //On indique qu'on a fini d'utiliser la pin GPIO 4.
+  led.unexport();
+})
+
+
 app.listen(port, (err) => {
   if (err) {
     return console.log('Erreur du serveur : ', err)
@@ -50,3 +68,11 @@ app.listen(port, (err) => {
   console.log('Le serveur écoute sur le port '+port+'\nRendez vous sur http://localhost:'+port);
 })
 
+
+
+
+
+
+process.on('SIGINT', () => {
+  led.unexport();
+});
